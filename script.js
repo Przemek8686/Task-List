fc04d6c6e171f5aa8503c23433c2b3695d5ff450
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-02-01 20:31:41 +0100

    const tasks added

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+console.log("hello");
 {
-    
+    const tasks = [
+        {
+            content: "trening",
+            done: false,
+        },
+        {
+            content: "zakupy",
+            done: true,
+        }
+    ]
 }
+
+
+const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+        htmlString += `<li class="list__task">
+      <button class="button__done js-done">
+      ${task.done ? "✓" : ""}
+      </button>
+      <span class="list__text ${task.done ? "list__text--done" : ""}">
+      ${task.content}
+      </span>
+      <button class="button__remove js-remove"> 🗑</button>
+      </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+};
+
+const init = () => {
+    render();
+    const form = document.querySelector(".js-form");
+
+}
+
+
+
