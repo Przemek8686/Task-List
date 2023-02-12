765f657ea86298e5b99bc1927fc79ef400b994ea
Przemek Krawczynski <1986krawiec1986@gmail.com>
2023-02-12 18:44:27 +0100

    refactor task function

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+let tasks = [];
+let hideDoneTasks = false;
 
-const tasks = [];
+
 
 const addNewTask = (newTaskContent) => {
-  tasks.push({
-    content: newTaskContent
-  });
+  tasks = [...tasks,{content:newTaskContent}];
+  }
 
   render();
-};
+
 
 const removeTask = (index) => {
-  tasks.splice(index, 1);
+  tasks = [
+    ...tasks.slice(0, index),
+    ...tasks.slice(index+1),
+];
   render();
 };
 
-const toggleTaskDone = (index) => {
-  tasks[index].done = !tasks[index].done;
+const toggleTaskDone = (taskIndex) => {
+ tasks = [
+  ...tasks.slice(0,taskIndex),
+  {...tasks[taskIndex],done: !tasks[taskIndex].done, },
+ ];
   render();
 };
 const bindEvents = () => {
